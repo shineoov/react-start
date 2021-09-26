7fbe3e6f193aee3667fddcfab11caddcfa3d70ea
shineoov <shineoov88@gmail.com>
2021-09-26 16:03:47 +0900

    클래스형 컴포넌트 state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import MyComponent from "./MyComponent";
+import Counter from "./Counter";
 
 const App = () => {
-    return <MyComponent name="shine" favoriteNumber={10}>child!</MyComponent>;
+    return <Counter/>;
 };
 
 
