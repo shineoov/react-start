ffe87dbb3dfc8494e73e4a1f0a52d86cbcdbaa67
shineoov <shineoov88@gmail.com>
2021-09-26 20:50:27 +0900

    EventHandler input 여러개

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -3,18 +3,20 @@ import React, {Component} from "react";
 class EventPractice extends Component {
 
     state = {
+        'username': '',
         'message': ''
     }
 
     handleChange = (e) => {
         this.setState({
-           message: e.target.value
+           [e.target.name]: e.target.value
         });
     }
 
     handleClick = () => {
-        alert(this.state.message);
+        alert(this.state.username + ': ' + this.state.message);
         this.setState({
+           username: '',
            message: ''
         });
     }
@@ -25,8 +27,15 @@ class EventPractice extends Component {
                 <h1>이벤트 연습!</h1>
                 <input
                     type="text"
+                    name="username"
+                    placeholder="사용자명"
+                    value={this.state.username}
+                    onChange={this.handleChange}
+                />
+                <input
+                    type="text"
                     name="message"
-                    placeholder="입력하세요~!"
+                    placeholder="메시지"
                     value={this.state.message}
                     onChange={this.handleChange}
                 />
