b4a42fa74ce1a07db1fd783079b0d5f4e51b0592
shineoov <shineoov88@gmail.com>
2021-09-26 15:28:50 +0900

    jsx if 문 대신 삼항연산자 사용

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const name = 'REACT';
+    return (
+        <>
+            {/* If */}
+            {name === 'REACT' ? <h1> 리액트~!</h1> : <h1> 리액트 X</h1>}
+            {name === 'REACT' ? <h2>작동 확인!</h2> : null}
+        </>
+    );
 }
 
 export default App;
