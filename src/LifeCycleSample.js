2b8d3213ce30e85689f21279356669bac0f5d6fb
shineoov <shineoov88@gmail.com>
2021-09-28 00:25:18 +0900

    error 코드 제거

--- src/LifeCycleSample.js
+++ src/LifeCycleSample.js
@@ -72,7 +72,6 @@ class LifeCycleSample extends Component {
 
         return (
             <div>
-                {this.props.missing.value}
                 <h1 style={style} ref={ref => this.myRef=ref}>
                     {this.state.number}
                 </h1>
